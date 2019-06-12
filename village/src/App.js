b3abf82ae32c7eebdb5b9cd896ff5636057da9f1
Dil Sombayeva <dilyasom@gmail.com>
2019-06-12 09:37:00 -0700

    implement POST req

--- village/src/App.js
+++ village/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+ constructor(props) {
+  super(props);
+  this.state = {
+   smurfs: [],
+  };
+ }
+ // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+
+ handlePost = newObj => {
+  axios
+   .post('http://localhost:3333/smurfs', newObj)
+   .then(res => {
+    console.log(res);
+    this.setState({ smurfs: res.data });
+   })
+   .catch(err => {
+    console.log(err);
+   });
+ };
+
  componentDidMount() {
   axios
    .get('http://localhost:3333/smurfs')
@@ -23,16 +37,16 @@ class App extends Component {
     console.log(err);
    });
  }
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-  render() {
-    return (
-      <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
-      </div>
-    );
-  }
+ // Notice what your map function is looping over and returning inside of Smurfs.
+ // You'll need to make sure you have the right properties on state and pass them down to props.
+ render() {
+  return (
+   <div className="App">
+    <SmurfForm handlePost={this.handlePost} />
+    <Smurfs smurfs={this.state.smurfs} />
+   </div>
+  );
+ }
 }
 
 export default App;
