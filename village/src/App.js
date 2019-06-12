0751ad76f8f6517acd39070423b187a620d41add
Dil Sombayeva <dilyasom@gmail.com>
2019-06-12 10:08:25 -0700

    Add two routes in App, one at '/' for Smurfs component,and one at /smurf-form for the form.

--- village/src/App.js
+++ village/src/App.js
@@ -42,8 +42,15 @@ class App extends Component {
  render() {
   return (
    <div className="App">
-    <SmurfForm handlePost={this.handlePost} />
-    <Smurfs smurfs={this.state.smurfs} />
+    <Route
+     exact
+     path="/"
+     render={props => <Smurfs smurfs={this.state.smurfs} />}
+    />
+    <Route
+     path="/smurf-form"
+     render={props => <SmurfForm handlePost={this.handlePost} {...props} />}
+    />
    </div>
   );
  }
