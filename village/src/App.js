42eb30abc2223e15855a5e7bd4f9d8cd3ccb4847
Dil Sombayeva <dilyasom@gmail.com>
2019-06-12 09:36:41 -0700

    implement GET req

--- village/src/App.js
+++ village/src/App.js
@@ -12,6 +12,17 @@ class App extends Component {
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+ componentDidMount() {
+  axios
+   .get('http://localhost:3333/smurfs')
+   .then(res => {
+    console.log(res);
+    this.setState({ smurfs: res.data });
+   })
+   .catch(err => {
+    console.log(err);
+   });
+ }
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
